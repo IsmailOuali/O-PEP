487369e89ebee0a51f159ff6dbc2d7f2b5067dc7
unknown <ismail,ouali@ump.ac.ma>
2023-11-27 20:48:31 +0100

    include config.php

--- produit-admin.php
+++ produit-admin.php
@@ -10,32 +10,60 @@
 
 <body>
     <div class="countainer pt-10 flex flex-col items-center justify-center">
-        <a class="w-3/5 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+        <a
+            class="w-3/5 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
             Ajouter Un produit
         </a>
-        <div class="product-table">
-            <table class="space-x-10">
-                <th>
-                    id
-                </th>
-                <th>
-                    Nom
-                </th>
-                <th>
-                    origine
-                </th>
-                <th>
-                    Taille
-                </th>
-                <th>
-                    Temperature
-                </th>
-                <th>
-                    Prix
-                </th>
-                <th>
-                    Id categorie
-                </th>
+
+
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg my-10">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Product name
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Color
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Category
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Price
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+
+
+
+                    <tr
+                        class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Apple MacBook Pro 17"
+                        </th>
+                        <td class="px-6 py-4">
+                            Silver
+                        </td>
+                        <td class="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td class="px-6 py-4">
+                            $2999
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            |
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Remove</a>
+                        </td>
+                    </tr>
+                    
+
+                </tbody>
             </table>
         </div>
 
